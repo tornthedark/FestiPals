976ec7d9c6068917aeed6da95b88144d56078c8d
petramihucz <petramihucz@gmail.com>
2016-11-30 14:04:47 +0100

    Header component with props
    props: title, description and icons + font awesome

---
+++ festipals/src/components/PageHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class PageHeader extends Component {
+  render() {
+    return (
+        <div className="container-fluid">
+          <div className="page-header">
+            <div className="container text-center">
+              <h1><i className={this.props.icon} aria-hidden="true"></i> {this.props.title}</h1>
+              <p>{this.props.description}</p>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
