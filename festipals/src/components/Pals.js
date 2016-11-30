952c1aa53e6fd869b60fd00dd087e6b067dd41b0
tornthedark <kimbach@hotmail.com>
2016-11-30 14:13:12 +0100

    Pals.js PalsPage.js

---
+++ festipals/src/components/Pals.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Pals extends Component {
+
+  render() {
+
+
+    return (
+      <div>
+      <div className="panel panel-default">
+        <div className="panel-body">
+          <div className="col-xs-3 palIcon">
+            <span className="fa fa-user fa-3x"></span>
+          </div>
+          <div className="col-xs-9">
+            <h3> Friend's Name</h3>
+            <p># common acts</p>
+          </div>
+
+        </div>
+      </div>
+
+      </div>
+    );
+  }
+}
--- festipals/src/components/PalsPage.js
+++ festipals/src/components/PalsPage.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Pals from './Pals';
 
 export default class PalsPage extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
+        <div className="container-fluid">
+          <div className="page-header">
+            <div className="container text-center">
+              <h1><span className="fa fa-user" aria-hidden="true"></span> Pals</h1>
+              <p>(Click on an act to see more details)</p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
         <h1>Your Pals</h1>
+        <p>Click on a Pal's name to see his or her personal schedule</p>
+          <Pals />
+        </div>
       </div>
     );
   }
