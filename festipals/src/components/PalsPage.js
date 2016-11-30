eb4db460accdabd3cb0d4b1fa2382ca2247cadd7
Kim Bach <tornthedark@gmail.com>
2016-11-30 17:19:41 +0100

    Start working on a for loop for pals

--- festipals/src/components/PalsPage.js
+++ festipals/src/components/PalsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PageHeader from './PageHeader.js';
 import Pals from './Pals.js';
+import SampleDataPals from './../samplePals.json';
 
 export default class PalsPage extends Component {
   render() {
@@ -10,7 +11,8 @@ export default class PalsPage extends Component {
         <div className="container">
           <h1>Your Pals</h1>
           <p>Click on a Pal&rsquo;s name to see his or her personal schedule</p>
-          <Pals />
+          {/*} looping through all the acts in the sample data file array to display the acts */}
+            {SampleDataPals.acts.map(function (pal) { return <Pals key={pal._id} id={pal._id} allPals={SampleDataPals} first_name={pal.first_name}  /> }) }
         </div>
       </div>
     );
