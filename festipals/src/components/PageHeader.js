b44b6b0fb2caaa0bbc983bda5e677c073bf75ac0
Kim Bach <tornthedark@gmail.com>
2016-12-01 23:56:24 +0100

    Fixed frontend problems (Pals.js / PalsPage.js)

--- festipals/src/components/PageHeader.js
+++ festipals/src/components/PageHeader.js
@@ -8,7 +8,7 @@ export default class PageHeader extends Component {
           <div className="page-header">
             <div className="container text-center">
               <h1><i className={this.props.icon} aria-hidden="true"></i> {this.props.title}</h1>
-              <p>{this.props.description}</p>
+              <p className="description">{this.props.description}</p>
             </div>
           </div>
         </div>
--- festipals/src/components/Pals.js
+++ festipals/src/components/Pals.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
+import SampleData from './../sampleData.json';
 
 export default class Pals extends Component {
 
   render() {
-
+    const { firstName, lastName, email} = this.props;
 
     return (
       <div>
-      <div className="panel panel-default">
-        <div className="panel-body">
-          <div className="col-xs-3 palIcon">
-            <span className="fa fa-user fa-3x"></span>
-          </div>
-          <div className="col-xs-9">
-            <h3> Friend&rsquo;s Name</h3>
-            <p># common acts</p>
+        <div className="panel panel-default">
+          <div className="panel-body">
+            <div className="col-xs-3 palIcon">
+              <span className="fa fa-user fa-3x"></span>
+            </div>
+            <div className="col-xs-9">
+              <h3>{firstName} {lastName}</h3>
+              <h4>{email}</h4>
+
+
+
+            </div>
           </div>
         </div>
-      </div>
 
       </div>
     );
