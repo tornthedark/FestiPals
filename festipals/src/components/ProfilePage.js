b8aa7d9ef6f7e7a366b16774a930290817f5fcbc
beltrame.valerie <beltrame.valerie@gmail.com>
2016-12-06 18:31:51 +0100

    added modals for editing and deleting an account

--- festipals/src/components/ProfilePage.js
+++ festipals/src/components/ProfilePage.js
@@ -35,7 +35,7 @@ export default class ProfilePage extends Component {
 
     return (
       <div>
-        <PageHeader icon="fa fa-user" title="Your Profile" description="(Edit or add some imformation about yourself)"/>
+        <PageHeader icon="fa fa-user" title="Your Profile" description="(Edit imformation about yourself and see your pal requests and act invites here)"/>
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-sm-6">
@@ -45,13 +45,13 @@ export default class ProfilePage extends Component {
               <p>E-mail: {currentUser.e_mail}</p>
             </div>
             <div className="col-xs-12 col-sm-6 profileButtons">
-              <button className="btn btn-primary btn-block btn-lg"><i className="fa fa-pencil"></i> Change Name</button>
+              <button data-toggle="modal" data-target="#changeName" className="btn btn-primary btn-block btn-lg"><i className="fa fa-pencil"></i> Change Name</button>
             </div>
             <div className="col-xs-12 col-sm-6 profileButtons">
-              <button className="btn btn-primary btn-block btn-lg"><i className="fa fa-pencil"></i> Change Password</button>
+              <button data-toggle="modal" data-target="#changePassword" className="btn btn-primary btn-block btn-lg"><i className="fa fa-pencil"></i> Change Password</button>
             </div>
             <div className="col-xs-12 col-sm-6 profileButtons">
-              <button className="btn btn-danger btn-block btn-lg"><i className="fa fa-trash"></i> Delete account </button>
+              <button data-toggle="modal" data-target="#deleteAccount" className="btn btn-danger btn-block btn-lg"><i className="fa fa-trash"></i> Delete account </button>
             </div>
           </div>
           <hr />
@@ -121,6 +121,74 @@ export default class ProfilePage extends Component {
             </div>
           </div>
         </div>
+
+        <div className="modal fade" id="changeName" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 className="modal-title" id="myModalLabel">Change your name</h4>
+              </div>
+              <div className="modal-body">
+                <p>Make sure to choose your name so that your pals know who you are.</p>
+                <div className="input-group nameInput">
+                  <span className="input-group-addon" id="basic-addon3">First Name</span>
+                  <input type="text" className="form-control" id="firstName" defaultValue={currentUser.first_name}/>
+                </div>
+                <div className="input-group">
+                  <span className="input-group-addon" id="basic-addon3">Last Name</span>
+                  <input type="text" className="form-control" id="lastName" defaultValue={currentUser.last_name}/>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-success"><i className="fa fa-pencil"></i> Save changes</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="modal fade" id="changePassword" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 className="modal-title" id="myModalLabel">Change your password</h4>
+              </div>
+              <div className="modal-body">
+                <p>Please enter your old password, so we see that this is really you:</p>
+                <input type="password" className="form-control nameInput" id="oldPassword"/>
+                <p>Please enter your new password twice:</p>
+                <input type="password" className="form-control nameInput" id="newPassword1"/>
+                <input type="password" className="form-control" id="newPassword2"/>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-success"><i className="fa fa-pencil"></i> Save changes</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="modal fade" id="deleteAccount" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 className="modal-title" id="myModalLabel">Delete your FestiPals account</h4>
+              </div>
+              <div className="modal-body">
+                <p>Are you sure you want to delete your account?</p>
+                <p>This action will delete all your saved content and can not be undone.</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-danger"><i className="fa fa-trash"></i> Delete </button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
