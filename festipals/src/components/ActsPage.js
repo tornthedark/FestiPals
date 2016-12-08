8064e75f1131285e281e5033c4f8557634a0b033
tornthedark <kimbach@hotmail.com>
2016-12-08 20:08:10 +0100

    Added Arts Sorter Component

--- festipals/src/components/ActsPage.js
+++ festipals/src/components/ActsPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Acts from './Acts.js';
+import ActsSorter from './ActsSorter';
 import PageHeader from './PageHeader.js';
 import SampleDataActs from './../sampleData.json';
 
@@ -9,6 +10,7 @@ export default class ActsPage extends Component {
     return (
       <div>
           <PageHeader icon="fa fa-music" title="Acts" description="(Click on an act to see more details)"/>
+          <ActsSorter />
           <div className="container">
         {/* looping through all the acts in the sample data file array to display the acts */}
           {SampleDataActs.acts.map(function (act) { return <Acts
---
+++ festipals/src/components/ActsSorter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Acts from './Acts.js';
+
+export default class ActsSorter extends Component {
+  render() {
+
+    return (
+      <div>
+        <div className="container">
+          <div className="col-xs-6">
+            <button>test</button>
+          </div>
+          <div className="col-xs-6">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
