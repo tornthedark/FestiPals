4d6df83dab722edc78b18731551faaea4e1fd558
tornthedark <kimbach@hotmail.com>
2016-12-09 14:04:35 +0100

    ActsSorter update

--- festipals/src/components/ActsSorter.js
+++ festipals/src/components/ActsSorter.js
@@ -5,28 +5,35 @@ export default class ActsSorter extends Component {
     return (
       <div>
       <div className="container">
-        <div className="btn-group col-xs-12">
-          <button type="button" className="btn btn-default" data-toggle="collapse"
-            href="#actsDays" aria-expanded="false" aria-controls="actsDays">
-            <span className="fa fa-calendar-o fa-3x" aria-hidden="true"></span>
-            <h3>Days</h3>
-          </button>
-          <button type="button" className="btn btn-default" data-toggle="collapse"
-          href="#actsSorterButtons" aria-expanded="false" aria-controls="actsSorterButtons">
-            <span className="fa fa-clone fa-rotate-270 fa-3x" aria-hidden="true"></span>
-            <h3>Filtering</h3>
-          </button>
-          <button type="button" className="btn btn-default">
-            <span className="fa fa-times fa-3x" aria-hidden="true"></span>
-            <h3>Reset</h3>
-          </button>
+        <div className="btn-group input-group btn-group-justified col-xs-12">
+          <div className="btn-group">
+            <button type="button" className="btn btn-default" data-toggle="collapse"
+              href="#actsDays" aria-expanded="false" aria-controls="actsDays">
+              <span className="fa fa-calendar-o fa-3x" aria-hidden="true"></span>
+              <h3>Days</h3>
+            </button>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn-default" data-toggle="collapse"
+            href="#actsSorterButtons" aria-expanded="false" aria-controls="actsSorterButtons">
+              <span className="fa fa-clone fa-rotate-270 fa-3x" aria-hidden="true"></span>
+              <h3>Filtering</h3>
+            </button>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn-default">
+              <span className="fa fa-times fa-3x" aria-hidden="true"></span>
+              <h3>Reset</h3>
+            </button>
+          </div>
         </div>
 
         <div className="collapse" id="actsDays">
           <div className="panel panel-default">
             <div className="panel-body">
               <h3>Select Days</h3>
-              <select className="dropdown">
+              <select className="form-control">
+                <option>All days</option>
                 <option>Monday ##/## 2017</option>
                 <option>Tuesday ##/## 2017</option>
                 <option>Wednesday ##/## 2017</option>
@@ -41,19 +48,25 @@ export default class ActsSorter extends Component {
 
         <div className="collapse" id="actsSorterButtons">
           <div className="card card-block">
-            <div className="btn-group col-xs-12">
-              <button type="button" className="btn btn-default">
-                <span className="fa fa-globe fa-3x" aria-hidden="true"></span>
-                <h3>Country</h3>
-              </button>
-              <button type="button" className="btn btn-default">
-                <span className="fa fa-home fa-3x" aria-hidden="true"></span>
-                <h3>Stage</h3>
-              </button>
-              <button type="button" className="btn btn-default">
-                <span className="fa fa-sort-alpha-asc fa-3x" aria-hidden="true"></span>
-                <h3>Name</h3>
-              </button>
+            <div className="btn-group input-group btn-group-justified col-xs-12">
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-globe fa-3x" aria-hidden="true"></span>
+                  <h3>Country</h3>
+                </button>
+              </div>
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-home fa-3x" aria-hidden="true"></span>
+                  <h3>Stage</h3>
+                </button>
+              </div>
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-sort-alpha-asc fa-3x" aria-hidden="true"></span>
+                  <h3>Name</h3>
+                </button>
+              </div>
             </div>
           </div>
         </div>
