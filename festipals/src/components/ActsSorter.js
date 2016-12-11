8245533dab8fcfe518c74c8bc7d3a113e761671e
tornthedark <kimbach@hotmail.com>
2016-12-11 15:19:11 +0100

    Collapse events
    Made collapse events for the days sorter and misc sorter sections of the
    acts list.
    Made a collapse event for the close all sorter sections button

--- festipals/src/components/ActsSorter.js
+++ festipals/src/components/ActsSorter.js
@@ -1,66 +1,105 @@
 import React, { Component } from 'react';
-import Acts from './Acts.js';
 
 export default class ActsSorter extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {daysButtonsCollapsed: 'collapse', sortButtonsCollapsed: 'collapse'};
+  }
 
+  collapseDaysButtons(event) {
+    if(this.state.daysButtonsCollapsed === "collapse") {
+      this.setState({daysButtonsCollapsed: 'collapse in'});
+    } else {
+      this.setState({daysButtonsCollapsed: 'collapse'});
+    }
+  }
+
+  collapseSortButtons(event) {
+    if(this.state.sortButtonsCollapsed === "collapse") {
+      this.setState({sortButtonsCollapsed: 'collapse in'});
+    } else {
+      this.setState({sortButtonsCollapsed: 'collapse'});
+    }
+  }
+
+  collapseAll(event) {
+    this.setState({daysButtonsCollapsed: 'collapse', sortButtonsCollapsed: 'collapse'});
+  }
+
+  render() {
     return (
       <div>
+      <div className="container">
+        <div className="btn-group input-group btn-group-justified col-xs-12">
+          <div className="btn-group">
+            <button type="button" className="btn btn-default" data-toggle="collapse"
+              href="#actsDaysButtons" aria-expanded="false" aria-controls="actsDays" onClick={this.collapseDaysButtons.bind(this)}>
+              <span className="fa fa-calendar-o fa-3x" aria-hidden="true"></span>
+              <h3>Days</h3>
+            </button>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn-default" data-toggle="collapse"
+            href="#actsSorterButtons" aria-expanded="false" aria-controls="actsSorterButtons" onClick={this.collapseSortButtons.bind(this)}>
+              <span className="fa fa-clone fa-rotate-270 fa-3x" aria-hidden="true"></span>
+              <h3>Filtering</h3>
+            </button>
+          </div>
+        </div>
 
-        <div className="container">
-
-          <a href="#" className="btn btn-default openall">open all</a> <a href="#" className="btn btn-default closeall">close all</a>
-          <hr />
-
-          <div className="panel-group" id="accordion">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h4 className="panel-title">
-                  <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                  Collapsible Group Item #1
-                  </a>
-                </h4>
-              </div>
-              <div id="collapseOne" className="panel-collapse collapse in">
-                <div className="panel-body">
-                  ONe Anim pariatur cliche reprehenderit, enim eiusmod high life
-                </div>
-              </div>
-            </div>
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                  <h4 className="panel-title">
-                    <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                    Collapsible Group Item #2
-                    </a>
-                  </h4>
-              </div>
-              <div id="collapseTwo" className="panel-collapse collapse">
-                <div className="panel-body">
-                Two Anim pariatur cliche reprehenderit,
-              </div>
-            </div>
-
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h4 className="panel-title">
-                  <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                    Collapsible Group Item #3
-                  </a>
-                </h4>
-              </div>
-              <div id="collapseThree" className="panel-collapse collapse">
-                <div className="panel-body">
-                Three Anim pariatur cliche reprehenderit,
-                </div>
-              </div>
-            </div>
-
-
-  
+        <div className={this.state.daysButtonsCollapsed} id="actsDaysButtons">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <h3>Select Days</h3>
+              <select className="form-control">
+                <option>All days</option>
+                <option>Monday ##/## 2017</option>
+                <option>Tuesday ##/## 2017</option>
+                <option>Wednesday ##/## 2017</option>
+                <option>Thursday ##/## 2017</option>
+                <option>Friday ##/## 2017</option>
+                <option>Saturday ##/## 2017</option>
+                <option>Sunday ##/## 2017</option>
+              </select>
             </div>
           </div>
         </div>
+
+        <div className={this.state.sortButtonsCollapsed} id="actsSorterButtons">
+          <div className="card card-block">
+            <div className="btn-group input-group btn-group-justified col-xs-12">
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-globe fa-3x" aria-hidden="true"></span>
+                  <h3>Country</h3>
+                </button>
+              </div>
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-home fa-3x" aria-hidden="true"></span>
+                  <h3>Stage</h3>
+                </button>
+              </div>
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-sort-alpha-asc  fa-3x" aria-hidden="true"></span>
+                  <h3>Stage</h3>
+                </button>
+              </div>
+              <div className="btn-group">
+                <button type="button" className="btn btn-default">
+                  <span className="fa fa-sort-alpha-desc  fa-3x" aria-hidden="true"></span>
+                  <h3>Stage</h3>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a type="button" href="#" className="btn btn-block btn-default"
+        onClick={this.collapseAll.bind(this)}>
+        <h4>Reset all filtering for acts</h4>
+        </a>
+      </div>
       </div>
     );
   }
